300883d0fdd9e96b336f294ed2021c25f1032b73
Padmaja018 <indiyumm23@gmail.com>
2025-11-25 16:54:22 +0530

    Updated code change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 // App.jsx
 import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import { ShoppingCart, X } from "lucide-react";
 import "../App.css";
